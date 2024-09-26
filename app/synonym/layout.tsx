11d7d4c88fa2ed0c5526b6abb5ee75d06b97089d
junaama <0naama0@gmail.com>
2024-09-26 07:43:29 -0400

    remove grid from layout

--- app/synonym/layout.tsx
+++ app/synonym/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({
     children: React.ReactNode;
 }>) {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] ">
+        <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] ">
             <Header />
             {children}
         </div>
