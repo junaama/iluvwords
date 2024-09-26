fdb815797d76d0afb4543e33d1fb0a8f93a9924d
junaama <0naama0@gmail.com>
2024-09-26 15:16:54 -0400

    add definitions for word of the day

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,40 @@
 'use client'
 import { Header } from '@/appcomponents/Header';
+import { useWordContext } from '@/context/word-context';
 import { Gamemodes } from '@/lib/constants';
 import Image from 'next/image'
 import Link from 'next/link';
 
 export default function Home() {
+  const { wordOfTheDay, definitionOfTheDay } = useWordContext()
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] ">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)] ">
       <Header />
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-
-        {Gamemodes.map((gamemode, index) => (
-          <div key={index} className="flex items-center gap-4 font-bold text-2xl">
-            <Image src={`/icons/${gamemode.icon}.svg`} alt="game icon" width="64" height="64" />
-            <Link href={gamemode.link}>
-              {gamemode.name}
-            </Link>
+      <main className="flex flex-col gap-8 row-start-2 items-center ">
+        <div className="flex flex-col items-center text-xl gap-4 p-8">
+          <div className="font-[family-name:var(--font-righteous)] text-4xl bg-gradient-to-r from-sky-300 via-amber-200 to-rose-500 inline-block text-transparent bg-clip-text">
+            {wordOfTheDay}
           </div>
-        ))}
+          <div>
+            {definitionOfTheDay}
+          </div>
+        </div>
+        <div className="container space-y-8 columns-1 md:columns-3 gap-8">
 
+
+          {Gamemodes.map((gamemode, index) => (
+            <div key={index} className="flex items-center gap-4 font-bold text-2xl">
+              <Image src={`/icons/${gamemode.icon}.svg`} alt="game icon" width="64" height="64" />
+              <Link href={gamemode.link}>
+                {gamemode.name}
+              </Link>
+            </div>
+          ))}
+        </div>
 
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center border-t border-sky-500 border-dashed">
-        <a
+        {/* <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="/"
           target="_blank"
@@ -30,7 +42,7 @@ export default function Home() {
         >
 
           FAQ
-        </a>
+        </a> */}
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://twitter.com/0naama"
--- context/word-context.tsx
+++ context/word-context.tsx
@@ -5,6 +5,7 @@ import React, { createContext, useState, useContext, useEffect } from 'react'
 type WordContextType = {
     wordOfTheDay: string
     setWordOfTheDay: React.Dispatch<React.SetStateAction<string>>
+    definitionOfTheDay: string
 }
 
 const WordContext = createContext<WordContextType | undefined>(undefined)
@@ -19,7 +20,7 @@ export const useWordContext = () => {
 
 export const WordProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [wordOfTheDay, setWordOfTheDay] = useState('')
-
+    const [definitionOfTheDay, setDefinitionOfTheDay] = useState('')
     useEffect(() => {
         // In a real application, this would fetch from an API or database
         // const words = ['COGNITION', 'SERENDIPITY', 'ELOQUENT', 'EPHEMERAL', 'LABYRINTHINE']
@@ -27,24 +28,56 @@ export const WordProvider: React.FC<{ children: React.ReactNode }> = ({ children
             "EPHEMERAL", "UBIQUITOUS", "SERENDIPITY", "ELOQUENT", "RESILIENT",
             "INNOVATIVE", "PARADIGM", "EMPATHY", "QUINTESSENTIAL", "ENIGMATIC",
             "PERSEVERANCE", "ALTRUISTIC", "AESTHETIC", "PRAGMATIC", "AMBIGUOUS",
-            "BENEVOLENT", "CACOPHONY", "DILIGENT", "EUPHORIA", 
+            "BENEVOLENT", "CACOPHONY", "DILIGENT", "EUPHORIA",
             "FACETIOUS", "LABYRINTHINE", "COGNITION", "RAMBUNCTIOUS",
-            "ONEROUS", "SEDENTARY", "CONGRUOUS", "EXCOGITATE", 
+            "ONEROUS", "SEDENTARY", "CONGRUOUS", "EXCOGITATE",
             "PREPONDERANCE", "VERIDICAL", "TORTILE"
         ];
 
-
+        const definitions = [
+            "(adj) Lasting for a very short time.", 
+            "(adj) Existing or being everywhere at the same time.", 
+            "(n) The occurrence of events by chance in a happy or beneficial way.", 
+            "(adj) Fluent and persuasive in writing or speech.", 
+            "(adj) Able to withstand or recover quickly from difficult conditions.", 
+            "(adj) Introducing new or original ideas.", 
+            "(n) A model of excellence or perfection.", 
+            "(n) The ability to understand and share the feelings of another.", 
+            "(adj) Representing the most perfect or typical example of something.", 
+            "(adj) Difficult to understand or interpret.", 
+            "(n) Persistence in a course of action in spite of difficulty or obstacles.", 
+            "(adj) Showing a disinterested and selfless concern for the well-being of others.", 
+            "(adj) Giving pleasure through beauty.", 
+            "(adj) Dealing with things in a practical and straightforward way.", 
+            "(adj) Open to more than one interpretation.", 
+            "(adj) Having or showing a kind and generous attitude towards others.", 
+            "(n) A harsh, unpleasant mixture of sounds.", 
+            "(adj) Showing careful and consistent effort or work.", 
+            "(n) A state of intense happiness or excitement.", 
+            "(adj) Treating serious issues with humor or in a way that is not meant to be taken literally.", 
+            "(adj) Complicated or confusing in a way that is difficult to understand.", 
+            "(n) The mental process of knowing, including aspects such as perception, reasoning, and memory.", 
+            "(adj) Uncontrollably boisterous and unruly.", 
+            "(adj) Causing a feeling of burden or oppression.", 
+            "(adj) Spending too much time sitting or doing very little physical activity.", 
+            "(adj) Being in harmony or agreement.", 
+            "(v) To think very carefully and thoroughly about something.", 
+            "(n) A state of being more important or influential than others.", 
+            "(adj) Conforming to facts; accurate.", 
+            "(adj) Twisted or turned in a way that is not normal or natural."
+        ];
         // Function to get word of the day
         // function getWordOfTheDay() {
-            const today = new Date();
-            const dayOfMonth = today.getDate() - 1; // 0-indexed
-            // return words[dayOfMonth % words.length];
+        const today = new Date();
+        const dayOfMonth = today.getDate() - 1; // 0-indexed
+        // return words[dayOfMonth % words.length];
         // }
         setWordOfTheDay(words[dayOfMonth % words.length])
+        setDefinitionOfTheDay(definitions[dayOfMonth % words.length])
     }, [])
 
     return (
-        <WordContext.Provider value={{ wordOfTheDay, setWordOfTheDay }}>
+        <WordContext.Provider value={{ wordOfTheDay, setWordOfTheDay, definitionOfTheDay }}>
             {children}
         </WordContext.Provider>
     )
