e1afdbafa04148d0b027bd9133107658fa40ec5b
junaama <0naama0@gmail.com>
2024-10-03 21:29:18 -0400

    prettier

--- lib/api.ts
+++ lib/api.ts
@@ -1,23 +1,23 @@
 export const isWordValid = async (word: string): Promise<boolean> => {
-    const res = await fetch(`/api/word?word=${word.toLowerCase()}`);
-    const data = await res.json();
-    return data.valid;
-  };
-  
-  export const isRhymeValid = async (word1: string, word2: string): Promise<boolean> => {
-    const res = await fetch(`/api/rhyme?word1=${word1.toLowerCase()}&word2=${word2.toLowerCase()}`);
-    const data = await res.json();
-    return data.isRhyme;
-  }
+  const res = await fetch(`/api/word?word=${word.toLowerCase()}`);
+  const data = await res.json();
+  return data.valid;
+};
 
-  export const isSynonymValid = async (word1: string, word2: string): Promise<boolean> => {
-    const res = await fetch(`/api/synonym?word1=${word1.toLowerCase()}&word2=${word2.toLowerCase()}`);
-    const data = await res.json();
-    return data.isSynonym;
-  }
+export const isRhymeValid = async (word1: string, word2: string): Promise<boolean> => {
+  const res = await fetch(`/api/rhyme?word1=${word1.toLowerCase()}&word2=${word2.toLowerCase()}`);
+  const data = await res.json();
+  return data.isRhyme;
+}
 
-  export const isAntonymValid = async (word1: string, word2: string): Promise<boolean> => {
-    const res = await fetch(`/api/antonym?word1=${word1.toLowerCase()}&word2=${word2.toLowerCase()}`);
-    const data = await res.json();
-    return data.isAntonym;
-  }
+export const isSynonymValid = async (word1: string, word2: string): Promise<boolean> => {
+  const res = await fetch(`/api/synonym?word1=${word1.toLowerCase()}&word2=${word2.toLowerCase()}`);
+  const data = await res.json();
+  return data.isSynonym;
+}
+
+export const isAntonymValid = async (word1: string, word2: string): Promise<boolean> => {
+  const res = await fetch(`/api/antonym?word1=${word1.toLowerCase()}&word2=${word2.toLowerCase()}`);
+  const data = await res.json();
+  return data.isAntonym;
+}
