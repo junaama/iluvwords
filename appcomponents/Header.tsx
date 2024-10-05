cbd134225a46d726e8c1ee376e307472ddb98dae
junaama <0naama0@gmail.com>
2024-10-05 18:34:39 -0400

    remove word dropdown from header

--- appcomponents/Header.tsx
+++ appcomponents/Header.tsx
@@ -1,8 +1,8 @@
 'use client'
 import { GameDropdown } from "./GameDropdown"
 import Link from "next/link"
-import { WordOfTheDayDropdown } from "./WordOfTheDayDropdown"
 import RulesModal from "@/appcomponents/RulesModal";
+import { useWordContext } from "@/context/word-context";
 
 type Props = {
     gameTitle?: string
@@ -12,25 +12,28 @@ type Props = {
 }
 
 export const Header = ({ gameTitle, rules, hideWordDropdown, hideGameDropdown }: Props) => {
+    const { wordOfTheDay, definitionOfTheDay } = useWordContext()
     return (
         <>
-            <header className="flex items-center py-4 w-full border-b border-sky-500 border-dashed">
-                <div className="flex-1">
-                    <h1 className={`font-[family-name:var(--font-righteous)] text-3xl`}>
-                        <Link href="/">iluvwords</Link>
-                    </h1>
-                </div>
-                
-                {!hideWordDropdown && (
-                    <div className="flex-1 flex justify-center">
-                        <WordOfTheDayDropdown />
+            <header className="flex flex-col  bg-gray-100 ">
+                <div className="flex justify-between items-center">
+                    <div className="w-1/2 " >
+                        <h1 className={`font-[family-name:var(--font-righteous)] text-3xl`}>
+                            <Link href="/">iluvwords</Link>
+                        </h1>
                     </div>
-                )}
-                
-                <div className="flex-1 flex gap-2" style={{justifyContent: "flex-end"}}>
-                    {!hideGameDropdown && <GameDropdown />}
-                    {gameTitle && rules && <RulesModal gameTitle={gameTitle} rules={rules} />}
+                    <div className="w-1/2  ">
+                        {!hideGameDropdown && <GameDropdown />}
+                        {gameTitle && rules && <RulesModal gameTitle={gameTitle} rules={rules} />}
+                    </div>
                 </div>
+                {!hideWordDropdown && <div className="w-full flex flex-col   gap-2 " >
+                    <div className="font-[family-name:var(--font-righteous)] text-2xl bg-gradient-to-r from-sky-300 via-orange-400 to-rose-500 inline-block text-transparent bg-clip-text">
+                        {wordOfTheDay}
+                    </div>
+                    <p className="mx-auto">{definitionOfTheDay}</p>
+                </div>
+                }
             </header>
         </>
     )
