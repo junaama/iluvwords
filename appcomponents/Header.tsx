c86a83b25f3dd31493132d0c5f2ede5932d847e8
junaama <0naama0@gmail.com>
2024-10-05 01:37:16 -0400

    update header alignment

--- appcomponents/Header.tsx
+++ appcomponents/Header.tsx
@@ -7,24 +7,29 @@ import RulesModal from "@/appcomponents/RulesModal";
 type Props = {
     gameTitle?: string
     rules?: string
+    hideWordDropdown?: boolean
 }
 
-export const Header = ({ gameTitle, rules }: Props) => {
-
+export const Header = ({ gameTitle, rules, hideWordDropdown }: Props) => {
     return (
         <>
-            <header className="flex justify-between items-center py-4 w-full border-b border-sky-500 border-dashed gap-4 ">
-                <h1 className={`font-[family-name:var(--font-righteous)] text-3xl`}><Link href="/">iluvwords</Link></h1>
-
-                <div className=" flex justify-center">
-                    <WordOfTheDayDropdown />
+            <header className="flex items-center py-4 w-full border-b border-sky-500 border-dashed">
+                <div className="flex-1">
+                    <h1 className={`font-[family-name:var(--font-righteous)] text-3xl`}>
+                        <Link href="/">iluvwords</Link>
+                    </h1>
                 </div>
-
-                <div className="flex gap-2">
+                
+                {!hideWordDropdown && (
+                    <div className="flex-1 flex justify-center">
+                        <WordOfTheDayDropdown />
+                    </div>
+                )}
+                
+                <div className="flex-1 flex gap-2" style={{justifyContent: "flex-end"}}>
                     <GameDropdown />
-                    {gameTitle && rules && <RulesModal gameTitle={gameTitle} rules={rules} /> }
+                    {gameTitle && rules && <RulesModal gameTitle={gameTitle} rules={rules} />}
                 </div>
-
             </header>
         </>
     )
