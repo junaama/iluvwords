976c600df880e17b0b082cbe591d61b655d35561
junaama <0naama0@gmail.com>
2024-10-05 07:01:56 -0400

    update rules for babel

--- app/babel/layout.tsx
+++ app/babel/layout.tsx
@@ -2,6 +2,8 @@ import SharedLayout from "@/appcomponents/SharedLayout"
 
 const babelRules = [
     "Create and connect anagrams from today's word of the day to drop all letters.",
+    "Example: ",
+    "If the Word of The Day is RAMBUNCTIOUS, valid anagrams and structures could be TRIM, COURT, BARN, TRAIN and so on.",
     "Come back daily and play again with new words!"
 ]
 
